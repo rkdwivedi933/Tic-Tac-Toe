8e3959a4555ae7aa7c0ed1481750401022af2f88
Rupesh Dwivedi <rkdwivedi933@gmail.com>
2025-04-02 23:46:01 +0530

    Updated Tic-Tac-Toe with reset button and bug fixes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const reset = document.querySelector(".Reset-Game");
+reset.addEventListener("click",function(){
+    resetBoard();
+    
+    
+})
 const cells = document.getElementsByClassName("cell");
-let currentPlayer = 'X'; 
 
+
+let currentPlayer = 'X';
 let winPattern = [
     [0,1,2], [3,4,5],[6,7,8],
     [0,4,8],[2,4,6],
@@ -14,9 +21,9 @@ for (let i = 0; i < cells.length; i++) {
             
             if(checkWinner ()){
                 alert(`Player ${currentPlayer} wins!`);
-                resetBoard();
+                console.log(currentPlayer,"win")
+                
             }
-
             if (currentPlayer === 'X') { 
                 currentPlayer = 'O'; 
             } else if (currentPlayer === 'O') {  
@@ -41,7 +48,7 @@ function checkWinner() {
     return false;
 }
 function resetBoard(){
-    for(let i = 0; i<cells.length(); i++){
+    for(let i = 0; i<cells.length; i++){
         cells[i].textContent = '';
 
     }
